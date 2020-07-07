294536ccd6514996f9534a601b430826b3c076b4
seven613 <seven613@126.com>
2020-07-07 17:44:21 +0800

    创建项目

--- app01/views.py
+++ app01/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+def users(request):
+    user_list = ['oldboy', 'alex']
+    return HttpResponse(json.dumps(user_list))
+
+
+class StudentView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('GET')
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST')
+
+    def put(self, request, *args, **kwargs):
+        return HttpResponse("PUT")
+
+    def delete(self, request, *args, **kwargs):
+        return HttpResponse("Delete")
